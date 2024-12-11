c2f4f055a3d54a4bffa3a1fb7aa9ee32fa976969
naveen-vsamy <naveentmsv@gmail.com>
2024-12-11 17:46:53 +0530

    pdf_func_updated

--- st.py
+++ st.py
@@ -9,7 +9,7 @@ import time
 with st.sidebar:
     st.title("Private Document")
    
-    file = st.file_uploader(label = "Select your text document")
+    file = st.file_uploader(label = "Select your .txt document")
     
     if file != None :
             #function to read data and upload to vector db
@@ -20,6 +20,16 @@ with st.sidebar:
                 vectorizer.document_uploader(file.read())
                 st.write("Document uploaded. Now you can ask questions related to your document to the chatbot")
 
+    file2 = st.file_uploader(label = "Select your .pdf document")
+    
+    if file2 != None :
+            #function to read data and upload to vector db
+           # st.write("Document uploaded. Now you can ask questions related to your document to the chatbot")
+
+        if st.button(label = "upload", type = "primary") :
+                #function to read data and upload to vector db
+                vectorizer.pdf_uploader(file2)
+                st.write("Document uploaded. Now you can ask questions related to your document to the chatbot")
 
 st.title("Assistant bot")
 
--- vectorizer.py
+++ vectorizer.py
@@ -1,24 +1,21 @@
-'''
-!pip install boto3
-!pip install langchain_pinecone
-!pip install langchain_aws
-'''
 import json
 import boto3
 import os
-import streamlit as st
-from langchain_aws import BedrockEmbeddings
+#from langchain_aws import BedrockEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone # Import Pinecone class
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_core.documents import Document
 from uuid import uuid4
-def document_uploader(document):
+import streamlit as st
 
-    os.environ['PINECONE_API_KEY']= st.secrets["PINECONE_API_KEY"]
-    os.environ['AWS_ACCESS_KEY_ID']= st.secrets["AWS_ACCESS_KEY_ID"]
-    os.environ['AWS_SECRET_ACCESS_KEY']= st.secrets["AWS_SECRET_ACCESS_KEY"]
-    os.environ['AWS_DEFAULT_REGION']=  st.secrets["AWS_DEFAULT_REGION"]
+os.environ['PINECONE_API_KEY']= st.secrets["PINECONE_API_KEY"]
+os.environ['AWS_ACCESS_KEY_ID']= st.secrets["AWS_ACCESS_KEY_ID"]
+os.environ['AWS_SECRET_ACCESS_KEY']= st.secrets["AWS_SECRET_ACCESS_KEY"]
+os.environ['AWS_DEFAULT_REGION']=  st.secrets["AWS_DEFAULT_REGION"]
+
+
+def document_uploader(document):
 
     embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1")
 
@@ -39,7 +36,7 @@ def document_uploader(document):
     # split documents into chunks
     text_splitter = CharacterTextSplitter(
         separator='\n',
-        chunk_size=3000,
+        chunk_size=300,
         chunk_overlap=30,
 
     )
@@ -51,3 +48,40 @@ def document_uploader(document):
     
     return True
 
+def pdf_uploader(file):
+
+    embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1")
+
+    # Create a Pinecone client instance
+    pinecone_client = Pinecone(api_key=os.environ["PINECONE_API_KEY"], environment="us-east-1")
+
+    # Get the index using the Pinecone client instance
+    index = pinecone_client.Index("resume")
+
+    # Initialize PineconeVectorStore with the index and embeddings
+    vector_store = PineconeVectorStore(index=index, embedding=embeddings)
+
+    # create a loader
+       
+    docs = []
+    doc = file.pages[0].extract_text()
+    l = len(file.pages)
+    
+    for i in range(0,len(file.pages)):
+        doc=file.pages[i].extract_text()
+        docs.append(Document(page_content = doc))
+
+    # split documents into chunks
+    text_splitter = CharacterTextSplitter(
+        separator='\n',
+        chunk_size=150,
+        chunk_overlap=25,
+
+    )
+    split_docs = text_splitter.split_documents(docs)
+
+    uuids = [str(uuid4()) for _ in range(len(split_docs))]
+
+    vector_store.add_documents(documents=split_docs, ids=uuids)
+    
+    return True
